69e061a3ce71485b2bf30802890c40920f934e7b
KohJiaJun <jjkoh95@gmail.com>
2019-08-29 16:10:00 +0800

    removed unnecessary constants

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ## gdriver
 
 go get github.com/jjkoh95/gdriver
-[Reference](https://godoc.org/github.com/jjkoh95/gdriver)
--- constants.go
+++
@@ -1,9 +0,0 @@
-package gdriver
-
-// Enum type value
-const (
-	DINT    = iota
-	DFLOAT  = iota
-	DSTRING = iota
-	DBOOL   = iota
-)
